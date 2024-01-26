38bbf6b50a9f188edb29b815fce29d3d502318c9
Arthur <94467283+avlrd@users.noreply.github.com>
2024-01-26 19:00:07 +0000

    Footer init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 	import { RouterView } from 'vue-router'
 	import Header from './components/header.vue'
+	import Footer from './components/footer.vue'
 </script>
 
 <template>
 	<Header />
 
 	<RouterView />
+
+	<Footer />
 </template>
---
+++ src/components/footer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<footer>
+		<div class="wrapper">
+			<span>Arthur Villard</span>
+			
+		</div>
+	</footer>
+</template>
+
+<style scoped>
+	footer {
+		background-color: rgb(0, 173, 181);
+		padding: 20px 0;
+	}
+
+	.wrapper {
+		margin: 0 auto;
+		padding: 0 20px;
+	}
+</style>
