9d6358244b43d0c9f77bd04cb9b3d8cca7ef805d
Arthur VILLARD <villardarthur@gmail.com>
2024-01-29 17:43:46 +0100

    Added iconoir and github icon

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,7 +5,9 @@
 	<footer>
 		<div class="wrapper">
 			<span>Arthur Villard</span>
-			
+			<div class="links">
+				<a href="https://github.com/avlrd/" target="_blank" rel="noopener noreferrer"><i class="iconoir-github"/></a>
+			</div>
 		</div>
 	</footer>
 </template>
@@ -14,10 +16,25 @@
 	footer {
 		background-color: rgb(0, 173, 181);
 		padding: 20px 0;
+		height: 10vh;
 	}
 
 	.wrapper {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 		margin: 0 auto;
 		padding: 0 20px;
+	}
+
+	.links {
+		display: flex;
+		align-items: center;
+	}
+
+	a {
+		text-decoration: none;
+		color: rgb(34, 40, 49);
+		margin-left: 20px;
 	}
 </style>
